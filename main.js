083838aa27f6ec0b1380a4a2530a5ad28d405f86
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-16 14:10:18 +0300

    last

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-function getRandomChinese(length){
-    return new Promise((resolve, reject) => {
-        if (length <= 0) {
-            reject('length is too short');
-        
-        } else {
-            let char = '';
-                for(let i = 0; i <= length -1; i++){
-                    const date = Date.now() + '';
+function getRandomChinese(length) {
+  return new Promise((resolve, reject) => {
+    if (length <= 0) {
+      reject("length is too short");
+    } else {
+      let char = "";
+      for (let i = 0; i <= length - 1; i++) {
+        const date = Date.now() + "";
 
-                    let dateFive = '';
-                    dateFive = date.substr(-5);
-                    char += String.fromCharCode(dateFive + i);
-                }
-            setTimeout(() => {
-              const element =  document.createElement('div');
-                resolve(element.innerText = 'result: ' + char);
-                document.body.append(element);
-              },50 * length)
-        }
-        })
-
-
-
+        let dateFive = "";
+        dateFive = date.substr(-5);
+        char += String.fromCharCode(dateFive + i);
+      }
+      setTimeout(() => {
+        renderResult();
+        resolve(
+          (document.querySelector(".cont").innerText = "result: " + char)
+        );
+      }, 50 * length);
+    }
+  });
 }
-getRandomChinese(5).then((char)=>{
-    }).catch((err)=>{
+function renderResult() {
+  const element = document.createElement("div");
+  element.className = "cont";
+  document.body.append(element);
+}
+getRandomChinese(5)
+  .then((char) => {})
+  .catch((err) => {
     console.log(err);
-    })
+  });
